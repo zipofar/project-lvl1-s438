4e2a9c9597f6ceef1209e394ed15ea13b48963a2
Zipofar <admin@zipofar.ru>
2019-02-04 16:56:42 +0300

    Fix step 2

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -2,6 +2,4 @@
 
 import Greeting from '..';
 
-console.log('Welcome to the Brain Games!\n');
-const userName = Greeting();
-console.log(`Hello, ${userName}!`);
+Greeting();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
-export default function greeting() {
+export default function () {
+  console.log('Welcome to the Brain Games!\n');
   const userName = readlineSync.question('May I have your name? ');
-  return userName;
+  console.log(`Hello, ${userName}!`);
 }
